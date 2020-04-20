b037781721965df1dfb3a34bc8a1631df3e234d0
Daniel Cohen <danielco232@gmail.com>
2020-04-20 12:21:44 +0300

    changed json to array

--- js/cookbook_func.js
+++ js/cookbook_func.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
     $("#login_button").click(function() {
-        let url = 'https://github.com/danielco232/cookbook/blob/master/users.json';
+        let url = 'https://github.com/danielco232/cookbook/blob/master/json/users.json';
         let request = new XMLHttpRequest();
         request.open('GET', url);
         request.responseType = 'json';
         request.send();
         request.onload = function() {
-        const users = request.response;
+            const users = request.response;
         };
         
         let username = $("#username").val();
-        let password = $("#password").val();
+        let password = users[1]["username"];
         
         alert(users[1]["username"]);
     });

ade464e01c6b0e2a21800b0264aca631a06652cd
Daniel Cohen <danielco232@gmail.com>
2020-04-22 14:53:41 +0300

    add recipes to json & html

--- cookbook_func.js
+++ cookbook_func.js
@@ -1,6 +1,15 @@
 /*if(window.location.href.split("/").pop() != "login.html" && !sessionStorage.getItem("username"))
     window.location.href = "login.html";
 */
+
+function create_string(str,i){
+    str += "<div class='post-preview'>";
+    str += "<h3 class='post-title'><a href='recipe.html' id='recipe'>" + recipes[i]["name"] + "</a></h3>";
+    str += "<p class='post-meta'>" + recipes[i]["calories"] + " calories per serving</p>";
+    str += "<hr></div>";
+    return str;
+}
+
 $(document).ready(function(){
     const url = "https://raw.githubusercontent.com/danielco232/cookbook/master/db.json";
     let request = new XMLHttpRequest();
@@ -29,19 +38,16 @@ $(document).ready(function(){
         });
 
         $(".category").click(function(){
-            window.location.href = "category.html";
+            //let str = "";
             let category = $(this).text();
-            $("#title").text(category);
-            
+            window.location.href="category.html";
+            $("#category_name").text(category);
            /* for(let i = 0; i < recipes.length; i++){
-                if (recipes[i]["category"] == category){
-                    $("#data").html("<div class='post-preview'>");
-                    $("#data").html("<h3 class='post-title'><a href='recipe.html' id='recipe'>" + recipes[i]["name"] + "</a></h3>");
-                    $("#data").html("<br>");
-                    $("#data").html("<p class='post-meta'>" + recipes[i]["calories"] + " calories per serving</p>");
-                    $("#data").html("<hr></div>");
-                }
-            }*/
+                if (recipes[i]["category"] == category)
+                    str = create_string(str,i)
+            }
+            $("#data").html(str);
+            */
         });
     }
 
